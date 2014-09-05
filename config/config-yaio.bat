c1d5351837b50169840fd62504094161ae7f6a14
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2014-09-05 23:49:23 +0200

    added JPAImport

--- config/config-yaio.bat
+++ config/config-yaio.bat
@@ -12,7 +12,7 @@ set YAIOCONFIGPATH=%BASEPATH%\..\config\
 set YAIOSCRIPTPATH=%BASEPATH%\..\sbin\
 set YAIORESPATH=%BASEPATH%\..\ressources\
 set YAIOVARPATH=%BASEPATH%\..\var\
-set CP="%BASEPATH%..\target\yaio-0.1.0.BUILD-SNAPSHOT-jar-with-dependencies.jar;%BASEPATH%..\lib\poi-extended.jar;%BASEPATH%..\lib\commons-cli-1.2.jar;%BASEPATH%..\target\classes"
+set CP="%BASEPATH%..\lib\poi-extended.jar;%BASEPATH%..\lib\commons-cli-1.2.jar;%BASEPATH%..\target\yaio-0.1.0.BUILD-SNAPSHOT-jar-with-dependencies.jar;"
 set JAVAOPTIONS=-Xmx512m -Xms128m -Dlog4j.configuration=file:%BASEPATH%..\config\log4j.properties
 set CFG="%BASEPATH%..\config"
 
@@ -30,6 +30,7 @@ set PROG_MM=de.yaio.extension.datatransfer.mindmap.JobNodes2MindMap
 set PROG_ICalN=de.yaio.extension.datatransfer.ical.JobNodes2ICal
 set PROG_CSVN=de.yaio.extension.datatransfer.csv.JobNodes2CSV
 set PROG_JSONN=de.yaio.extension.datatransfer.json.JobNodes2JSON
+set PROG_JPAN=de.yaio.extension.datatransfer.jpa.JobNodes2JPA
 set PROG_DIFF=
 set PROG_WINMERGE="C:\ProgrammePortable\PortableApps\PortableApps\WinMergePortable\WinMergePortable.exe"
 
