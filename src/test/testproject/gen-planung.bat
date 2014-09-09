91b61e1296704f82d61487492a2ca21fdc33109e
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2014-09-10 01:04:23 +0200

    updated the testproject

--- src/test/testproject/gen-planung.bat
+++ src/test/testproject/gen-planung.bat
@@ -6,37 +6,32 @@ set BASEPATH=%YAIOBASEPATH%
 
 set PLANFILEBASE=test
 set PLANNAME=TestPlanung
+set PLANSYSUID=DT201401231236052431
 
-goto shortway
-call %BASEPATH%gen-planung-fromwiki.bat %PLANDIR% %PLANFILEBASE% %PLANNAME% --onlyifstate
+goto regen-from-excel
+:gen-all
+rem gen all
+call %BASEPATH%gen-planung-fromwiki.bat %PLANDIR% %PLANFILEBASE% %PLANNAME% --onlyifstate %PLANSYSUID%
 call %BASEPATH%checkdiffs %PLANDIR%%PLANFILEBASE%.wiki %PLANDIR%%PLANFILEBASE%-real.new.wiki
 
-call %BASEPATH%importJPAFromPPL.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
-call %BASEPATH%genICalFromPPL.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
-call %BASEPATH%genCsvFromPPL.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
-call %BASEPATH%genMMFromPPL.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
-call %BASEPATH%genHtmlFromPPL.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
-call %BASEPATH%genJSONFromPPL.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
-call %BASEPATH%genExcelFromPPL.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
-rem call %BASEPATH%genWikiFromPPL.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
-
-cp %PLANFILEBASE%-real.xls %PLANFILEBASE%-regen.xls
-call %BASEPATH%genPPLFromExcel.bat %PLANDIR% %PLANFILEBASE%-regen %PLANNAME%
-call %BASEPATH%genWikiFromPPL.bat %PLANDIR% %PLANFILEBASE%-regen %PLANNAME%
-goto end
-
-:shortway
-call %BASEPATH%gen-planung-fromwiki.bat %PLANDIR% %PLANFILEBASE% %PLANNAME% --onlyifstate
-call %BASEPATH%checkdiffs %PLANDIR%%PLANFILEBASE%.wiki %PLANDIR%%PLANFILEBASE%-real.new.wiki
-
-rem bei WikiOnly rest ignoerieren 
-if NOT "%WIKIONLY%" EQU "" goto end
-
-rem alles generieren
+:gen-changelog
+rem gen changelog
 call %BASEPATH%gen-planungchangelog.bat %PLANDIR% %PLANFILEBASE%-real %PLANNAME%
 
+:gen-lebenslauf
+rem gen lebenslauf
+set GEN_SRC=ppl
+set SRC_OPTIONS=--sourcetype ppl
 grep -E "Lebenslauf bis 2013" %PLANDIR%%PLANFILEBASE%-real.ppl > %PLANDIR%%PLANFILEBASE%lebenslauf.ppl
 call %BASEPATH%genHtmlFromPPL.bat %PLANDIR% %PLANFILEBASE%lebenslauf %PLANNAME%
+
+:regen-from-excel
+rem gen regen from excel
+set GEN_SRC=ppl
+set SRC_OPTIONS=--sourcetype ppl
+cp %PLANDIR%%PLANFILEBASE%-real.xls %PLANDIR%%PLANFILEBASE%-regen.xls
+call %BASEPATH%genPPLFromExcel.bat %PLANDIR% %PLANFILEBASE%-regen %PLANNAME%
+call %BASEPATH%genWikiFromPPL.bat %PLANDIR% %PLANFILEBASE%-regen %PLANNAME%
 
 :end
 
