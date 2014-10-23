85b63e457e58d7501fd072cb3d44666810cc6130
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2014-10-23 18:26:30 +0200

    added: startscript for linux (on my demoserver :-)

---
+++ config/config-yaio.sh
@@ -0,0 +1,61 @@
+#/!/bin/bash
+echo off
+
+# set CONFIG
+export BASEPATH=${1}
+export FLGWP=${2}
+
+# Gen Wiki-Only
+export PARSEONLY=
+# parse ppl from wiki an generate 
+export FLG_PARSE_PPL_FROM_WIKI=
+# import the ppl to db
+export FLG_IMPORT_PPL_TO_DB=
+# the generate src: ppl or jpa
+export GEN_SRC=jpa
+# the masternode
+export MASTERNODEID="MasterplanMasternode1"
+export IMPORT_OPTIONS_JPA="--addnodestosysuid ${MASTERNODEID}"
+
+# set Path
+export YAIOCONFIGPATH=${BASEPATH}/../config/
+export YAIOSCRIPTPATH=${BASEPATH}/../sbin/
+export YAIORESPATH=${BASEPATH}/../ressources/
+export YAIOVARPATH=${BASEPATH}/../var/
+export YAIOAPP=${BASEPATH}../target/yaio-0.1.0.BUILD-SNAPSHOT-shaded.jar
+export CP="${YAIOAPP}"
+export CFGFILE=${BASEPATH}../config/application.properties
+export CFG="--config ${CFGFILE}" 
+export JAVAOPTIONS="-Xmx768m -Xms128m -Dspring.config.location=file:${CFGFILE} -Dlog4j.configuration=file:${BASEPATH}../config/log4j.properties"
+
+
+# set progs
+export PROG_WP=de.yaio.extension.datatransfer.wiki.JobParseWiki
+export PROG_WN=de.yaio.extension.datatransfer.wiki.JobNodes2Wiki
+export PROG_EX=de.yaio.extension.datatransfer.excel.JobNodes2Excel
+export PROG_EP=de.yaio.extension.datatransfer.excel.JobParseExcel
+export PROG_HN=de.yaio.extension.datatransfer.html.JobNodes2Html
+export PROG_MM=de.yaio.extension.datatransfer.mindmap.JobNodes2MindMap
+export PROG_ICalN=de.yaio.extension.datatransfer.ical.JobNodes2ICal
+export PROG_CSVN=de.yaio.extension.datatransfer.csv.JobNodes2CSV
+export PROG_JSONN=de.yaio.extension.datatransfer.json.JobNodes2JSON
+export PROG_JPAN=de.yaio.extension.datatransfer.jpa.JobNodes2JPA
+export PROG_APP=de.yaio.app.Application
+export PROG_RECALC=de.yaio.jobs.JobRecalcNodes
+export PROG_DIFF=
+export PROG_WINMERGE="C:/ProgrammePortable/PortableApps/PortableApps/WinMergePortable/WinMergePortable.exe"
+
+export FILE_DELIM="-"
+
+echo ${CP}
+
+# set Config
+export NEWID_OPTIONS="-pathiddb ${YAIOVARPATH}//nodeids.properties"
+export PARSER_OPTIONS="${NEWID_OPTIONS} ${FLGWP}"
+export PARSER_OPTIONS_EXCEL="${NEWID_OPTIONS}"
+export OUTPUT_OPTIONS="--calcsum -U 3 -p -i -s -d -t -intend 80 -intendli 2 -l" 
+# normal (recalcsum)
+export OUTPUT_OPTIONS_WIKI=" -U 3 -p -i -s -d -t -intend 80 -intendli 2 -l" 
+# with calcsum export OUTPUT_OPTIONS_WIKI="--calcsum -U 3 -p -i -s -d -t -intend 80 -intendli 2 -l" 
+# export OUTPUT_OPTIONS="--calcsum -U 3 -p -i -s -d -t -intend 80 -intendli 2 -l -shownometadata -shownosysdata"
+
