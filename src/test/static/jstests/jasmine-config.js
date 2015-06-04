1206b0cb2e8f15c63796401813e14ed656a832b2
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2015-06-04 20:35:03 +0200

    improved test-structure

--- src/test/static/jstests/jasmine-config.js
+++
@@ -1,3 +0,0 @@
-jasmine.getFixtures().fixturesPath = 'base/src/test/static/jstests/fixtures/';
-jasmine.getStyleFixtures().fixturesPath = 'base/src/test/static/jstests/fixtures/';
-jasmine.getJSONFixtures().fixturesPath = 'base/src/test/static/jstests/fixtures/';
---
+++ src/test/static/unit/jasmine-config.js
@@ -0,0 +1,3 @@
+jasmine.getFixtures().fixturesPath = 'base/src/test/static/unit/fixtures/';
+jasmine.getStyleFixtures().fixturesPath = 'base/src/test/static/unit/fixtures/';
+jasmine.getJSONFixtures().fixturesPath = 'base/src/test/static/unit/fixtures/';
