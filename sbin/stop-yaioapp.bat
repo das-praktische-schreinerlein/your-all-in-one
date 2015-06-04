28e252a9e87aa3aec39892c4f7db13289efb224b
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2015-06-04 22:13:17 +0200

    added stop-script for windows

---
+++ sbin/stop-yaioapp.bat
@@ -0,0 +1,34 @@
+echo off
+rem <h4>FeatureDomain:</h4>
+rem     Collaboration
+rem <h4>FeatureDescription:</h4>
+rem     stop the app with rest-services
+rem <h4>Syntax:</h4>
+rem     PROG
+rem <h4>Example:</h4>
+rem     cd D:\public_projects\yaio\yaio
+rem     sbin\stop-yaioapp
+rem 
+rem @package de.yaio
+rem @author Michael Schreiner <michael.schreiner@your-it-fellow.de>
+rem @category Collaboration
+rem @copyright Copyright (c) 2011-2014, Michael Schreiner
+rem @license http://mozilla.org/MPL/2.0/ Mozilla Public License 2.0
+
+rem set pathes
+set YAIOSCRIPTPATH=%~dp0
+set YAIOBASEPATH=%YAIOSCRIPTPATH%
+set BASEPATH=%YAIOBASEPATH%
+set YAIOCONFIGPATH=%YAIOSCRIPTPATH%..\config\
+
+rem init config
+call %YAIOCONFIGPATH%\config-yaio.bat %YAIOSCRIPTPATH%
+
+set CMD=java %JAVAOPTIONS% -cp %CP% %PROG_APPPROPAGATOR% %CFG%
+echo "stop-apppropagator: %CMD%"
+taskkill /FI "WINDOWTITLE eq C:\Windows\system32\java.exe"
+
+rem add --debug option to see the startprocess of spring-boot
+set CMD=java %JAVAOPTIONS% -cp %CP% %PROG_APP% %CFG% %NEWID_OPTIONS%
+echo "stop-yaioapp: %CMD%"
+taskkill /FI "WINDOWTITLE eq C:\Windows\system32\java.exe"
