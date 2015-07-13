ab1de01e2c2821d4aa467af006453d0fb6ea12bb
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2015-07-13 13:15:02 +0200

    improved tests: use logincheck

--- src/test/static/e2e/login/login.po.js
+++ src/test/static/e2e/login/login.po.js
@@ -23,7 +23,7 @@ var YAIOLoginPage = function() {
                 browser.params.yaioConfig.browserSize.height);
         browser.get(browser.params.yaioConfig.yaioBaseAppUrl + '/login');
         return browser;
-    }
+    };
 
     /**
      * submit valid login-page
@@ -54,7 +54,66 @@ var YAIOLoginPage = function() {
     me.doLogout = function () {
         browser.get(browser.params.yaioConfig.yaioBaseAppUrl + '/logout');
         return browser;
-    }
+    };
     
+    /**
+     * check login and open pageurl if session is ok or login passes
+     * @param {Striong} url    url to open after login-check
+     * @returns {Promise}  - promise
+     */
+    me.loginAndOpenPage = function(url) {
+        return me.checkLogin()
+            .then(function loadUrl() {
+                return browser.get(url);
+            });
+    };
+
+    /**
+     * check login and do login if no session
+     * @returns {Promise}  - promise
+     */
+    me.checkLogin = function() {
+        var defer = protractor.promise.defer();
+        
+        // define checkResponseHandler
+        var checkResponseHandler = function(source){
+            if (! source || ! (source.search('authenticated') > 0)) {
+                // on error do Login
+                console.error("session not ok: do login");
+                me.doLogin()
+                    .then(function loginPassed() {
+                        protractor.utils.waitUntilElementPresent($('#front-content-left'), protractor.utils.CONST_WAIT_ELEMENT);
+                        defer.fulfill(true);
+                    }, function loginFailed () {
+                        console.error("login failed");
+                        defer.reject(false);
+                    })
+            } else {
+                // open Frontpage
+                browser.get(browser.params.yaioConfig.yaioBaseAppUrl)
+                    .then(function () {
+                        defer.fulfill(true);
+                    });
+            }
+        }
+           
+        // check the current Userstate 
+        browser.ignoreSynchronization = true;
+        browser.get(browser.params.yaioConfig.yaioBaseUrl + '/user/current')
+            .then(function sucess() {
+                browser.getPageSource()
+                    .then(function setSource(source) {
+                        browser.ignoreSynchronization = false;
+                        checkResponseHandler(source);
+                    }, function sourceError() {
+                        console.error("error while getting source");
+                        browser.ignoreSynchronization = false;
+                        checkResponseHandler(source);
+                    })
+            }); 
+        
+        // Return a promise so the caller can wait on it for the request to complete
+        return defer.promise;
+    };
 };
 module.exports = YAIOLoginPage;
