628c49cc31b4e88039e502cb38ac6132f1c2e5b7
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2015-07-27 18:26:10 +0200

    improved package-structure - moved statictests to src/test/javascript

--- src/test/static/unit/jasmine-config.js
+++
@@ -1,6 +0,0 @@
-jasmine.getFixtures().fixturesPath = 'base/src/test/static/unit/fixtures/';
-jasmine.getStyleFixtures().fixturesPath = 'base/src/test/static/unit/fixtures/';
-jasmine.getJSONFixtures().fixturesPath = 'base/src/test/static/unit/fixtures/';
-
-yaioAppBase = YaioAppBase(); 
-yaioAppBase.publishDetectorStyles();
