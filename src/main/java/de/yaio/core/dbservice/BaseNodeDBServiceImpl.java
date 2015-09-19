c41d048bc49fdbbd304433bb1b2d97f49e19bff5
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2015-09-19 03:23:44 +0200

    added new searchOptions

--- src/main/java/de/yaio/core/dbservice/BaseNodeDBServiceImpl.java
+++ src/main/java/de/yaio/core/dbservice/BaseNodeDBServiceImpl.java
@@ -54,26 +54,26 @@ public class BaseNodeDBServiceImpl implements BaseNodeDBService {
     protected static final Map<String, String> CONST_AVAILIABLE_SORTS = new HashMap<String, String>();
     static {
         CONST_AVAILIABLE_SORTS.put("default", "");
-        CONST_AVAILIABLE_SORTS.put("createdUp", "sysCreateDate asc");
-        CONST_AVAILIABLE_SORTS.put("createdDown", "sysCreateDate desc");
-        CONST_AVAILIABLE_SORTS.put("istEndeUp", "istChildrenSumEnde asc");
-        CONST_AVAILIABLE_SORTS.put("istEndeDown", "istChildrenSumEnde desc");
-        CONST_AVAILIABLE_SORTS.put("istStartUp", "istChildrenSumStart asc");
-        CONST_AVAILIABLE_SORTS.put("istStartDown", "istChildrenSumStart desc");
-        CONST_AVAILIABLE_SORTS.put("lastChangeUp", "sysChangeDate asc");
-        CONST_AVAILIABLE_SORTS.put("lastChangeDown", "sysChangeDate desc");
+        CONST_AVAILIABLE_SORTS.put("createdUp", sortNullBehind("sysCreateDate", "asc"));
+        CONST_AVAILIABLE_SORTS.put("createdDown", sortNullBehind("sysCreateDate", "desc"));
+        CONST_AVAILIABLE_SORTS.put("istEndeUp", sortNullBehind("istChildrenSumEnde", "asc"));
+        CONST_AVAILIABLE_SORTS.put("istEndeDown", sortNullBehind("istChildrenSumEnde", "desc"));
+        CONST_AVAILIABLE_SORTS.put("istStartUp", sortNullBehind("istChildrenSumStart", "asc"));
+        CONST_AVAILIABLE_SORTS.put("istStartDown", sortNullBehind("istChildrenSumStart", "desc"));
+        CONST_AVAILIABLE_SORTS.put("lastChangeUp", sortNullBehind("sysChangeDate", "asc"));
+        CONST_AVAILIABLE_SORTS.put("lastChangeDown", sortNullBehind("sysChangeDate", "desc"));
         CONST_AVAILIABLE_SORTS.put("nameUp", "name asc");
         CONST_AVAILIABLE_SORTS.put("nameDown", "name desc");
         CONST_AVAILIABLE_SORTS.put("nodeNumberUp", "metaNodePraefix asc, metaNodeNummer asc");
         CONST_AVAILIABLE_SORTS.put("nodeNumberDown", "metaNodePraefix desc, metaNodeNummer desc");
-        CONST_AVAILIABLE_SORTS.put("planEndeUp", "planEnde asc");
-        CONST_AVAILIABLE_SORTS.put("planEndeDown", "planEnde desc");
-        CONST_AVAILIABLE_SORTS.put("planStartUp", "planStart asc");
-        CONST_AVAILIABLE_SORTS.put("planStartDown", "planStart desc");
-        CONST_AVAILIABLE_SORTS.put("planChildrenSumEndeUp", "planChildrenSumEnde asc");
-        CONST_AVAILIABLE_SORTS.put("planChildrenSumEndeDown", "planChildrenSumEnde desc");
-        CONST_AVAILIABLE_SORTS.put("planChildrenSumStartUp", "planChildrenSumStart asc");
-        CONST_AVAILIABLE_SORTS.put("planChildrenSumStartDown", "planChildrenSumStart desc");
+        CONST_AVAILIABLE_SORTS.put("planEndeUp", sortNullBehind("planEnde", "asc"));
+        CONST_AVAILIABLE_SORTS.put("planEndeDown", sortNullBehind("planEnde", "desc"));
+        CONST_AVAILIABLE_SORTS.put("planStartUp", sortNullBehind("planStart", "asc"));
+        CONST_AVAILIABLE_SORTS.put("planStartDown", sortNullBehind("planStart", "desc"));
+        CONST_AVAILIABLE_SORTS.put("planChildrenSumEndeUp", sortNullBehind("planChildrenSumEnde", "asc"));
+        CONST_AVAILIABLE_SORTS.put("planChildrenSumEndeDown", sortNullBehind("planChildrenSumEnde", "desc"));
+        CONST_AVAILIABLE_SORTS.put("planChildrenSumStartUp", sortNullBehind("planChildrenSumStart", "asc"));
+        CONST_AVAILIABLE_SORTS.put("planChildrenSumStartDown", sortNullBehind("planChildrenSumStart", "desc"));
         CONST_AVAILIABLE_SORTS.put("typeUp", "type asc");
         CONST_AVAILIABLE_SORTS.put("typeDown", "type desc");
         CONST_AVAILIABLE_SORTS.put("workflowStateUp", "workflowState asc");
@@ -470,5 +470,9 @@ public class BaseNodeDBServiceImpl implements BaseNodeDBService {
         }
         
         return query;
+    }
+    
+    protected static String sortNullBehind(final String fieldName, final String order) {
+        return "CASE WHEN " + fieldName + " IS NULL THEN 1 ELSE 0 END, " + fieldName + " " + order;
     }
 }
