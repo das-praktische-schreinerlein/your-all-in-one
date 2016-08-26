a7c5bb85084765e01d7184019f606ff00d677eca
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2016-08-26 18:54:35 +0200

    new app-version

--- yaio-app-webapp/Gruntfile.js
+++ yaio-app-webapp/Gruntfile.js
@@ -89,6 +89,7 @@
                     vendorDestBase + 'js/highlightjs/highlight.pack.js',
                     vendorDestBase + 'js/select2/select2.full.min.js',
                     vendorDestBase + 'js/slimbox2/slimbox2.js',
+                    vendorDestBase + 'js/spin.js/spin.min.js',
                     vendorDestBase + 'js/toastr/toastr.min.js',
 // loaded standalone because of problems
 //              vendorDestBase + 'js/mermaid/mermaid.full.js',
