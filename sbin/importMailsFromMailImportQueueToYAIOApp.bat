e60fbd79a827b63f50d28da431a3be26abab82d9
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2016-09-17 18:00:52 +0200

    added batchscripts for mailimport from queuedir

--- sbin/importMailsFromMailImportQueueToYAIOApp.bat
+++ sbin/importMailsFromMailImportQueueToYAIOApp.bat
@@ -4,7 +4,7 @@ rem     Collaboration
 rem <h4>FeatureDescription:</h4>
 rem     imports mailfiles from mailimportqueue to running yaio-app
 rem <h4>Syntax:</h4>
-rem     PROG
+rem     PROG REPEATEVERYSECONDS
 rem <h4>Example:</h4>
 rem     cd D:\public_projects\yaio\yaio
 rem     sbin\importMailsFromMailImportQueueToYAIOApp.bat
@@ -14,6 +14,8 @@ rem @author Michael Schreiner <michael.schreiner@your-it-fellow.de>
 rem @category Collaboration
 rem @copyright Copyright (c) 2011-2014, Michael Schreiner
 rem @license http://mozilla.org/MPL/2.0/ Mozilla Public License 2.0
+
+set REPEATEVERYSECONDS=%1
 
 rem set pathes
 set YAIOSCRIPTPATH=%~dp0
@@ -29,8 +31,18 @@ set SAVEDPWD=%cd%
 set LOCALBASEPATH=%~dp0\..
 cd %LOCALBASEPATH%
 
-set CMD=call %YAIOSCRIPTPATH%\importMailsFromFolderToYAIOApp.bat %YAIOSCRIPTPATH%\..\mailimportqueue\ DELETE
+:startloop
+set CMD=call %YAIOSCRIPTPATH%\importMailsFromFolderToYAIOApp.bat %YAIOSCRIPTPATH%\..\queues\mailimportqueue\ DELETE
 echo %CMD%
 %CMD%
 
+if "%REPEATEVERYSECONDS%" EQU "" goto endloop
+
+if %REPEATEVERYSECONDS% GTR 0 (
+    timeout /T %REPEATEVERYSECONDS% /nobreak
+    goto startloop
+  )
+)
+
+:endloop
 cd %SAVEDPWD%
